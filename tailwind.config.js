0d2a4dbec1e554fc5fa81f7638d392f120426a77
hieppnguyen <n.s.hiep@ttdesignco.com>
2023-10-19 16:35:46 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0F1B4C',
+        secondary: '#0689FF',
+        text1: '#000336',
+        text2: '#4F5361',
+        text3:'#687690'
+      }
+    },
+  },
+  plugins: [],
+};
